85aceffc5115d6e560a5ea96a3731834590d0142
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2025-02-19 09:42:07 -0300

    alterando a configuração e o proposito da aplicação

--- agents/agent_getdata_AAPL.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-from agents.main import Console
-
-def get_market_data(ticker="AAPL"):
-    Console.console.print("[bold yellow]Coletando dados do ticker AAPL...[/bold yellow]")
-    stock = yf.Ticker(ticker)
-    hist = stock.history(period="1d")
-    Console.console.print(hist)
